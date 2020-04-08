ec97b8fcff05bdb75f9210ed033a8bd3a095c36e
geb <gebura@poolp.org>
2020-04-08 16:49:14 +0200

    Revert "ensure push.sh errors are properly passed to travis"
    This reverts commit 073a8aab73619faf7d8f03d4786c1bb844f628b3.

--- push.sh
+++ push.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-git config --global user.email "travis@travis-ci.org" && \
-git config --global user.name "Travis CI" && \
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-git checkout -b gh-pages && \
-git add tails.pdf && \
-git commit -m "Travis build: $TRAVIS_BUILD_NUMBER" && \
+git checkout -b gh-pages
+git add tails.pdf
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-pages https://${GITHUB_TOKEN}@github.com/mgoessen/tails-workshop.git > /dev/null 2>&1 &&\
-git push --quiet --set-upstream origin-pages gh-pages &&
-exit $?
+git remote add origin-pages https://${GITHUB_TOKEN}@github.com/mgoessen/tails-workshop.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages gh-pages
