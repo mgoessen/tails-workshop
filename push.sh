b116596d5f93a3761c034ad73222a47acd1c6deb
geb <gebura@poolp.org>
2020-04-08 15:35:09 +0200

    corrected token var name

--- push.sh
+++ push.sh
@@ -7,5 +7,5 @@ git checkout -b gh-pages
 git add tails.pdf
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-pages https://${GH_TOKEN}@github.com/mgoessen/tails-workshop.git > /dev/null 2>&1
+git remote add origin-pages https://${GITHUB_TOKEN}@github.com/mgoessen/tails-workshop.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-pages gh-pages
